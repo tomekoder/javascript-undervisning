abeb9c29ceee65b144bfe8469b98a6c9ecafe427
tomekoder <tomekoder@gmail.com>
2024-11-11 12:04:05 +0100

    buttons, script

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
-const userName = 'Tom';
-const userAge = 23;
-const isOnline = true;
+function person() {
+    console.log("heii");
 
-console.log(userName, userAge, isOnline);
+    const userName = "Tom";
+    const userAge = 23;
+    let isOnline = true;
+
+    isOnline = false;
+
+    console.log(userName, userAge, isOnline);
+
+}
+
+//person();
+
+const viewPersonInfo = document.querySelector("#viewPersonInfo");
+console.log(viewPersonInfo);
+
+viewPersonInfo.addEventListener("click", person);
+
+function simpleCalculator(num1, num2) {
+    const sum = num1 + num2;
+
+    console.log(sum);
+}
+
+//simpleCalculator(5, 4);
+
+const viewCalculation = document.querySelector("#viewCalculation");
+console.log(viewCalculation);
+
+//viewCalculation.addEventListener("click", simpleCalculator(5, 4));
+
+viewCalculation.addEventListener("click", function simpleCalculator(num1, num2) {
+    const sum = num1 + num2;
+
+    console.log(sum);
+    //simpleCalculator(4, 5);
+}
+)
+
